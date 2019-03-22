de91e2c799bd0a305541a4e1668d5d131bc6215b
YS <Yerzhan Serikbay>
2019-03-22 21:52:51 +0800

    Section4

---
+++ Section5.34(Traffic Lights Game)/Section5.34(Traffic Lights Game)/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Section5.34(Traffic Lights Game)
+//
+//  Created by YS on 3/22/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
