1131be7bb8687ac9736a42a0ff77b99a8f282076
YS <Yerzhan Serikbay>
2019-03-28 17:40:04 +0800

    Section15

---
+++ Section15(Camera Application)/Section15(Camera Application)/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Section15(Camera Application)
+//
+//  Created by YS on 3/28/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Section15(Camera Application)/Section15(Camera Application)Tests/Section15_Camera_Application_Tests.m
@@ -0,0 +1,37 @@
+//
+//  Section15_Camera_Application_Tests.m
+//  Section15(Camera Application)Tests
+//
+//  Created by YS on 3/28/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Section15_Camera_Application_Tests : XCTestCase
+
+@end
+
+@implementation Section15_Camera_Application_Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
