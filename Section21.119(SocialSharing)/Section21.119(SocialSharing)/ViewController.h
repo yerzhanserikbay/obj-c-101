e5e07db95d942865c5b97e9dbd57490f0afa30af
YS <Yerzhan Serikbay>
2019-03-30 03:00:36 +0800

    Section21.119(SocialSharing)

---
+++ Section21.119(SocialSharing)/Section21.119(SocialSharing)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Section21.119(SocialSharing)
+//
+//  Created by YS on 3/30/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Section21.119(SocialSharing)/Section21.119(SocialSharing)/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Section21.119(SocialSharing)
+//
+//  Created by YS on 3/30/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
