e8a9cb052bcfe9de2f230185e9940c7a45ce6ce5
YS <Yerzhan Serikbay>
2019-04-01 18:15:51 +0800

    Section24

---
+++ Section24.130(DeviceDetection)/Section24.130(DeviceDetection)/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Section24.130(DeviceDetection)
+//
+//  Created by YS on 4/1/19.
+//  Copyright © 2019 YS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)buttonPressed:(id)sender;
+
+@end
+
